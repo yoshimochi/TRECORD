cb0a6d12c8e644af69677b837a167e2c0f52a037
yoshimochi <olive.oil6319@gmail.com>
2021-08-02 13:17:26 +0000

    フォロー機能実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,8 +8,33 @@ class User < ApplicationRecord
   has_many :posts, dependent: :destroy
   has_many :favorites, dependent: :destroy
   has_many :post_comments, dependent: :destroy
-  has_many :set_events, dependent: :destroy
   has_many :records, dependent: :destroy
   has_many :user_tags
   has_many :tags, through: :user_tags
+
+   # フォロー側
+  has_many :relationships, dependent: :destroy
+  has_many :followings, through: :relationships, source: :follower
+
+  # フォロワー側
+  has_many :passive_relationships, class_name: 'Relationship', foreign_key: 'follower_id', dependent: :destroy
+  has_many :followers, through: :passive_relationships, source: :user
+
+  # フォローする
+  def follow(other_user)
+    return if self == other_user
+    relationships.find_or_create_by!(follower: other_user)
+  end
+
+
+  # フォローの確認
+  def following?(user)
+    followings.include?(user)
+  end
+
+  # フォローの解除
+  def unfollow(relathinoship_id)
+    relationships.find(relathinoship_id).destroy!
+  end
+
 end
