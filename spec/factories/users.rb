a244f7ad3f05b2d89bebac89d79303b9f937ae1f
yoshimochi <olive.oil6319@gmail.com>
2021-08-10 12:18:22 +0000

    [Add]投稿のテスト

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,8 +1,8 @@
 FactoryBot.define do
-  factory :user do
-    sequence(:name) { |n| "person#{n}" }
-    sequence(:email) { |n| "person#{n}@example.com" }
-    password { password }
-    password_confirmation { password }
+  factory :user, class: User do
+    sequence(:name) { |n| 'person#{n}' }
+    sequence(:email) { |n| 'person#{n}@example.com' }
+    password { 'password' }
+    password_confirmation { 'password' }
   end
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -2,8 +2,36 @@
 
 require 'rails_helper'
 
-describe 'モデルのテスト' do
-  it "有効な投稿内容の場合保存されるか" do
-    expect(FactoryBot.build(:post)).to be_valid
-    
+RSpec.describe 'Postモデルのテスト', type: :model do
+  describe 'バリデーションのテスト' do
+    let(:user) { create(:user) }
+    let!(:post) { build(:post, user_id: user.id) }
+
+    subject { test_post.valid? }
+    let(:test_post) { post }
+
+    context 'bodyカラム' do
+      it '空欄ではないこと' do
+        post.body = ""
+        is_expected.to eq false;
+      end
+    end
+  end
+  describe 'アソシエーションのテスト' do
+    context 'userモデルとの関係' do
+      it 'N:1となっている' do
+        expect(Post.reflect_on_association(:user).macro).to eq :belongs_to
+      end
+    end
+    context 'post_commentモデルとの関係' do
+      it '1:Nとなっている' do
+        expect(Post.reflect_on_association(:post_comments).macro).to eq :has_many
+      end
+    end
+    context 'favoriteモデルとの関係' do
+      it '1:Nとなっている' do
+        expect(Post.reflect_on_association(:favorites).macro).to eq :has_many
+      end
+    end
+  end
 end
