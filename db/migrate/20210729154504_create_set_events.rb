85087e1e12113d9cbf156e22b688a28fc7789d8c
yoshimochi <olive.oil6319@gmail.com>
2021-08-18 15:23:42 +0000

    RM計算のメソッドをモデルに記述

--- db/migrate/20210729154504_create_set_events.rb
+++ db/migrate/20210729154504_create_set_events.rb
@@ -4,7 +4,7 @@ class CreateSetEvents < ActiveRecord::Migration[5.2]
       t.float "weight", default: 0
       t.integer "rep", default: 0
       t.integer "set", default: 0
-      t.float ""
+      t.float "max"
       t.integer "training_record_id"
 
       t.timestamps
